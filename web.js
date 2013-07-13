09f7fa9b6dea86ef1cbff92c2da9e3a81f0b108c
John Cotter <John.Cotter.3@gmail.com>
2013-07-13 04:58:22 +0000

    Updated web.js - hopefully fixed encoding error try 4

--- web.js
+++ web.js
@@ -3,10 +3,10 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var buf = new Buffer(256,"utf-8");
+var buf = new Buffer(256);
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSync('index.html'),"utf-8"));
+  response.send(buf.toString("utf-8",fs.readFileSync('index.html')));
 });
 
 var port = process.env.PORT || 5000;
